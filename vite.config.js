750ac70648c36f046f88026aede1185e2bdaef0a
Project Manager <project-manager@example.com>
2026-01-07 23:16:01 +0300

    ضبط المسارات النهائية وتفعيل النشر التلقائي

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    target: 'esnext'
-  },
+  base: '/Account-Verification/',
   server: {
     host: true,
     port: 5173,
